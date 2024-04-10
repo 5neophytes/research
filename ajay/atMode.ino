4fd9f0e9fd5e7a6babe25f063ed3f7f0f7f179a3
ajay nagpal <130251489+eco-bone@users.noreply.github.com>
2024-04-10 21:15:25 +0530

    Create atMode.ino
    Code to run to put hc-05 into AT mode

---
+++ ajay/atMode.ino
@@ -0,0 +1,26 @@
+# this code is to put the hc-05 into AT mode 
+# after putting into at mode, AT commands are to be put into the Serial Monitor | all commands will be replied with OK if working properly
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(10, 11); // RX | TX
+
+void setup()
+{
+  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(9, HIGH);
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  BTSerial.begin(38400);  // HC-05 default speed in AT command more
+}
+
+void loop()
+{
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
